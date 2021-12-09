acb53a7c0f07f36d2ca91deb8a12fcca3077a0fa
milijanadj <milijana.djordjevic1998@gmail.com>
2021-12-09 18:50:56 +0100

    project initialized

---
+++ src/main/java/sep/firstbank/FirstBankApplication.java
@@ -0,0 +1,13 @@
+package sep.firstbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sep/firstbank/FirstBankApplicationTests.java
@@ -0,0 +1,13 @@
+package sep.firstbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
